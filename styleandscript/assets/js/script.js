cbd81595fa40d79b17f0d95cd6bb9563d859d675
Neil Micarandayo <109055177+NeilMicarandayo@users.noreply.github.com>
2024-10-07 13:15:11 +0800

    Update script.js

--- styleandscript/assets/js/script.js
+++ styleandscript/assets/js/script.js
@@ -1,39 +1,28 @@
 'use strict';
 
-
-
-// element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
-
-// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
-
-// reviews variables
 const reviewsItem = document.querySelectorAll("[data-reviews-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
 const reviewsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
 for (let i = 0; i < reviewsItem.length; i++) {
 
   reviewsItem[i].addEventListener("click", function () {
@@ -49,13 +38,9 @@ for (let i = 0; i < reviewsItem.length; i++) {
 
 }
 
-// add click event to modal close button
 modalCloseBtn.addEventListener("click", reviewsModalFunc);
 overlay.addEventListener("click", reviewsModalFunc);
 
-
-
-// custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
@@ -63,7 +48,6 @@ const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -75,7 +59,6 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -94,7 +77,6 @@ const filterFunc = function (selectedValue) {
 
 }
 
-// add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -115,16 +97,13 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 
-// contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
-    // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
@@ -134,13 +113,9 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
-
-
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
